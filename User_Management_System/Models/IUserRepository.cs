422620ef53d088cb20ab84c1be42c4c9f07c3e64
Aanchal Bisht <aanchal.b@droisys.com>
2024-08-27 17:58:11 +0530

    changes from my project till now

---
+++ User_Management_System/Models/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace User_Management_System.Models
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int userId);
+        public DataTable getUserDetails(string uname, string pass);
+        public DataTable getUserList();
+    }
+}
