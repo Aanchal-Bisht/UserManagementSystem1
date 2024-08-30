88d96574d47760afb708c372386e9123b9e4215e
Parth Bajaj <parthbajaj162@gmail.com>
2024-08-30 14:30:53 +0530

    assembly files

--- User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
+++ User_Management_System/obj/Debug/net8.0/User_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175ce57f6b6ba8dcfae6d0c207171bbcb7f27064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f26820f66940af41ae4c3892fc420e7dd57a70d")]
 [assembly: System.Reflection.AssemblyProductAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
