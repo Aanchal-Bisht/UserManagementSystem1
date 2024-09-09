ff1be638776479cddf7b468adc89b996fcd1f9af
Aanchal Bisht <aanchal.b@droisys.com>
2024-09-09 16:16:54 +0530

    logs implementation

---
+++ User_Management_System/Program.cs
@@ -0,0 +1,28 @@
+﻿using User_Management_System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<CustomConfig>(builder.Configuration.GetSection("CustomConfig"));
+builder.Services.Configure<Custom>(builder.Configuration.GetSection("Custom"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
