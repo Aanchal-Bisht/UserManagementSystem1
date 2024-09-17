a1f63de1c4ca90597b11b7b9fa82a4decd9d7000
Parth Bajaj <parthbajaj162@gmail.com>
2024-09-17 15:59:59 +0530

    LogReadController added..

---
+++ UMSAPI/Controllers/LogReadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace UMSAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogReadController : Controller
+    {
+       private IOptions<LogFileConfig> _logConfig;
+        public LogReadController(IOptions<LogFileConfig> logConfig)
+        {
+            _logConfig = logConfig;
+        }
+        [HttpGet("LogRead")]
+        public string Get()
+        {
+            string logFilePath = _logConfig.Value.LogFilePath;
+            string path = Path.Combine(logFilePath, "log.txt");
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = reader.ReadToEnd();
+                return line;
+            }
+
+
+        }
+
+    }
+}
