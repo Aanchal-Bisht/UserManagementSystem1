42115d0cb49b22a095e52e103c0884a89691a183
Parth Bajaj <parthbajaj162@gmail.com>
2024-09-19 15:18:42 +0530

    changes made in delete user controller and LogWriter file .

--- UMSAPI/LogWriter.cs
+++ UMSAPI/LogWriter.cs
@@ -29,24 +29,24 @@ namespace UMSAPI
                 }
             }
         }
-        public static void DeleteLogWrite(string logMessage, string path)
-        {
-            m_exePath = path;
-            if (string.IsNullOrEmpty(m_exePath) == false)
-            {
-                if (!File.Exists(m_exePath + "\\" + "log.txt"))
-                    File.Create(m_exePath + "\\" + "log.txt");
-                try
-                {
-                    using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
-                        AppendLog(logMessage, w);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
+        //public static void DeleteLogWrite(string logMessage, string path)
+        //{
+        //    m_exePath = path;
+        //    if (string.IsNullOrEmpty(m_exePath) == false)
+        //    {
+        //        if (!File.Exists(m_exePath + "\\" + "log.txt"))
+        //            File.Create(m_exePath + "\\" + "log.txt");
+        //        try
+        //        {
+        //            using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+        //                AppendLog(logMessage, w);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+        //}
         private static void AppendLog(string logMessage, TextWriter txtWriter)
         {
             try
