edf1aad235d6d2a917880ad474ea2c453230fc70
Parth Bajaj <parthbajaj162@gmail.com>
2024-09-24 12:24:35 +0530

    removed DeleteLogWrite method from the Log writer class....

--- UMSAPI/LogWriter.cs
+++ UMSAPI/LogWriter.cs
@@ -6,7 +6,7 @@ namespace UMSAPI
     public static class LogWriter
     {
         private static string m_exePath = string.Empty;
-        public static void LogWrite(string logMessage ,string path)
+        public static void LogWrite(string logMessage, string path)
         {
 
             // m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -29,24 +29,24 @@ namespace UMSAPI
                 }
             }
         }
-        public static void DeleteLogWrite(string logMessage, string path)
-        {
-            m_exePath = path;
-            if (string.IsNullOrEmpty(m_exePath) == false)
-            {
-                if (!File.Exists(m_exePath + "\\" + "log.txt"))
-                    File.Create(m_exePath + "\\" + "log.txt");
-                try
-                {
-                    using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
-                        AppendLog(logMessage, w);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-        }
+        //public static void DeleteLogWrite(string logMessage, string path)
+        //{
+        //    m_exePath = path;
+        //    if (string.IsNullOrEmpty(m_exePath) == false)
+        //    {
+        //        if (!File.Exists(m_exePath + "\\" + "log.txt"))
+        //            File.Create(m_exePath + "\\" + "log.txt");
+        //        try
+        //        {
+        //            using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+        //                AppendLog(logMessage, w);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+        //}
 
         //public static void RegisterLogWrite(string logMessage, string path)
         //{
@@ -96,11 +96,11 @@ namespace UMSAPI
         {
             try
             {
-              //  txtWriter.Write("\r\nLog Entry : ");
-                txtWriter.WriteLine("{0} {1} {2}",DateTime.Now.ToString("yyyy-MMM-dd"), DateTime.Now.ToLongTimeString(), logMessage);
-              //  txtWriter.WriteLine("  :");
-               // txtWriter.WriteLine("  :{0}", logMessage);
-              //  txtWriter.WriteLine("-------------------------------");
+                //  txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("{0} {1} {2}", DateTime.Now.ToString("yyyy-MMM-dd"), DateTime.Now.ToLongTimeString(), logMessage);
+                //  txtWriter.WriteLine("  :");
+                // txtWriter.WriteLine("  :{0}", logMessage);
+                //  txtWriter.WriteLine("-------------------------------");
             }
             catch (Exception ex)
             {
