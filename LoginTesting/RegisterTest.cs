7b87c9280c86c6036db2fd372057a02d80765920
Parth Bajaj <parthbajaj162@gmail.com>
2024-10-09 10:48:25 +0530

    1.added automated test suite for register UI 2.upadted register api url.

---
+++ LoginTesting/RegisterTest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V127.Network;
+using OpenQA.Selenium.Support.UI;
+namespace LoginTesting
+{
+    public class RegisterTest
+    {
+        [Test]
+        public void Test()
+        {
+             IWebDriver driver=new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5272/User/Register");
+            driver.FindElement(By.Id("username")).SendKeys("riya");
+            driver.FindElement(By.Id("email")).SendKeys("riya12@gmail.com ");
+            driver.FindElement(By.Id("password")).SendKeys("1225");
+            driver.FindElement(By.Id("dob")).SendKeys("2006-5-22");
+            SelectElement genderSelect = new SelectElement(driver.FindElement(By.Id("Gender")));
+            genderSelect.SelectByValue("");
+            SelectElement departmentSelect = new SelectElement(driver.FindElement(By.Id("Department")));
+            departmentSelect.SelectByValue("HR");
+            driver.FindElement(By.Name("phone")).SendKeys("");
+            driver.FindElement(By.Id("RegUser")).Submit();
+            WebDriverWait Wait = new WebDriverWait(driver, new TimeSpan(0, 0, 50));
+            Console.WriteLine("hello");
+            Assert.Pass();
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
--- LoginTesting/obj/Debug/net8.0/LoginTesting.AssemblyInfo.cs
+++ LoginTesting/obj/Debug/net8.0/LoginTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21352b9f0db2ed86e03b73a300cc319e0e8f351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5acded4cbc8c74e461c240ad81ad30ee384ecd3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
