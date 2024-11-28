11625e40db379e56cd3bfe135e6ad3cdf2609d39
Parth Bajaj <parthbajaj162@gmail.com>
2024-11-28 09:34:58 +0530

    UpdationV2Controller created , UpdateUsersBody class created for getting the data and some changes made in RegistrationV2Controller...

--- UMSAPI/UpdateUserBody.cs
+++ UMSAPI/UpdateUserBody.cs
@@ -1,15 +1,15 @@
 ﻿namespace UMSAPI
 {
-    public class RegisterUser
+    public class UpdateUserBody
     {
-
+        public string? UserId { get; set; }
         public string? UserName { get; set; }
+
         public string? Password { get; set; }
         public string? Email { get; set; }
         public string? ContactNo { get; set; }
         public string? Gender { get; set; }
         public string? DOB { get; set; }
         public string? DeptId { get; set; }
-      
     }
 }
